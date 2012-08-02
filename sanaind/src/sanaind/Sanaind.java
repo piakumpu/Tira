d17860a553a5c18995ed3f377817f1e70f6edc3e
Pia Kumpulainen <pia.kumpulainen@cs.helsinki.fi>
2012-08-02 22:51:58 +0300

    tira

---
+++ sanaind/src/sanaind/Sanaind.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanaind;
+
+import java.util.Scanner;
+/**
+ *
+ * @author piakumpu
+ */
+public class Sanaind {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static Scanner lukija = new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ sanaind/test/sanaind/SanaindTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanaind;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author piakumpu
+ */
+public class SanaindTest {
+    
+    public SanaindTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Sanaind.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Sanaind.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
